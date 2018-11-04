398f8c182a7718476914a962988383146a421b8f
Solomon Ezeobika <solomonzbk@gmail.com>
2018-11-04 17:02:27 +0100

    Initialize repo

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const middleware = {
+    requireAuthentication: (req, res, next) => {
+        console.log('private route hit!');
+        next();
+    },
+    logger: (req, res, next) => {
+        const date = new Date();
+        console.log(`Request: ${date} ${req.method} ${req.originalUrl}`);
+        next();
+    }
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, (req, res) => {
+    res.send("About Us");
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, (req, res, next) => {
+    console.log(`Express server started on port ${PORT}!`);
+});
